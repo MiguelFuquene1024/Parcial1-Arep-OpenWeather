d439d2310ebdbba6a55f38deeee07c53beef6883
Miguel Angel Fuquene Arias <migueplay_10@hotmail.com>
2021-09-08 14:43:20 -0500

    Conection porrt 35000  and socket

--- src/main/java/edu/escuelaing/arep/openweather/ServerWeather.java
+++ src/main/java/edu/escuelaing/arep/openweather/ServerWeather.java
@@ -5,10 +5,78 @@
  */
 package edu.escuelaing.arep.openweather;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketImpl;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author Miguel
  */
 public class ServerWeather {
+
+    private static final ServerWeather instance = new ServerWeather();
+    private static final HashMap<String, String> contentList = new HashMap<String, String>();
+
+    public static ServerWeather getInstance() {
+        contentList.put("html", "text/html");
+        contentList.put("css", "text/css");
+        contentList.put("js", "text/javascript");
+        contentList.put("jpeg", "image/jpeg");
+        contentList.put("jpg", "image/jpg");
+        contentList.put("png", "image/png");
+        return instance;
+    }
+
+    public ServerWeather() {
+
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(getPort());
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        boolean running = true;
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Ready to deploy");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            
+        }
+        serverSocket.close();
+
+    }
+
     
+    private int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 35000; //returns default port if heroku-port isn't set (i.e. on localhost)    
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer.getInstance().start();
+    }
+
 }
